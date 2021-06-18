be7b65010a3818ec061042e4c65ad8791a8f94ae
SoorajSNBlaze333 <sooraj@skcript.com>
2021-06-18 22:39:03 +0530

    Missing Typings

--- components/terminal/Window.tsx
+++ components/terminal/Window.tsx
@@ -1,10 +1,20 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { KeyboardEvent, useEffect, useRef, useState } from 'react';
 import { ArrowRight } from '../ui/Icons';
 import { EventEmitter } from '../../libs/events';
 
-const helpCommands = [
+interface LineCommandProps {
+  command: string,
+  reply: string | null,
+}
+
+interface ReplyProps {
+  index: number,
+  reply: string | null,
+}
+
+const helpCommands: string[] = [
   "about",
   "work",
   "projects",
@@ -16,15 +26,15 @@ const helpCommands = [
 ];
 
 const Window = () => {
-  const helpCommandIndexRef = useRef(0)
-  const inputRef = useRef(null);
-  const [lineCommands, setLineCommands] = useState([{ command: '', reply: null }]);
+  const helpCommandIndexRef = useRef<number>(0)
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [lineCommands, setLineCommands] = useState<LineCommandProps[]>([{ command: '', reply: null }]);
   
-  const handleKey = (e, index) => {
-    if (e.keyCode === 38) {
+  const handleKey = (e: KeyboardEvent, index: number) => {
+    if (e.code === '38') {
       e.preventDefault();
       const commandIndex = Number(helpCommandIndexRef.current)
-      inputRef.current.value = helpCommands[commandIndex];
+      if (inputRef.current) inputRef.current.value = helpCommands[commandIndex];
       setLineCommands((lc) => {
         let newLC = [...lc];
         newLC[index].command = helpCommands[commandIndex];
@@ -32,19 +42,19 @@ const Window = () => {
       })
       helpCommandIndexRef.current = (helpCommandIndexRef.current + 1) % helpCommands.length;
     }
-    if (e.keyCode === 40) {
+    if (e.code === '40') {
       e.preventDefault();
       helpCommandIndexRef.current -= 1;
       if (helpCommandIndexRef.current < 0) helpCommandIndexRef.current = helpCommands.length - 1;
       const commandIndex = Number(helpCommandIndexRef.current)
-      inputRef.current.value = helpCommands[commandIndex];
+      if (inputRef.current) inputRef.current.value = helpCommands[commandIndex];
       setLineCommands((lc) => {
         let newLC = [...lc];
         newLC[index].command = helpCommands[commandIndex];
         return newLC;
       })
     }
-    if (e.key === "Enter" || e.keyCode === 13) {
+    if (e.key === "Enter" || e.code === '13') {
       EventEmitter.dispatch('execCommand', { command: lineCommands[index].command,  index })
       if ((lineCommands[index].command === "clear")) setLineCommands([{ command: '', reply: null }])
       else setLineCommands((lc) => {
@@ -61,12 +71,16 @@ const Window = () => {
     }
   };
 
-  const handleReply = ({ index, reply }) => {
+  const handleReply = ({ index, reply }: ReplyProps) => {
     setLineCommands((lc) => {
       let newLC = [...lc];
       newLC[index].reply = reply; 
       return newLC;
     });
+  }
+
+  const handleFocus = () => {
+    if (inputRef.current) return inputRef.current.focus();
   }
 
   useEffect(() => {
@@ -76,7 +90,7 @@ const Window = () => {
     }
   }, []);
 
-  const renderLine = (line, index) => (
+  const renderLine = (line: LineCommandProps, index: number) => (
     <div key={index}>
       <div className="min-h-6 h-auto w-full flex">
         <div className="h-full w-16 flex items-center">
@@ -92,7 +106,7 @@ const Window = () => {
           ref={inputRef}
           className="h-auto w-full bg-transparent outline-none terminal-input text-white lowercase font-semibold"
           autoFocus
-          onKeyDown={(e) => handleKey(e, index)}
+          onKeyDown={(e: KeyboardEvent) => handleKey(e, index )}
         />) : (<div className="h-auto w-full text-white lowercase font-semibold">
           {line.command}
         </div>)}
@@ -110,7 +124,7 @@ const Window = () => {
   return (
     <div style={{ height: 'calc(100% - 60px)' }} className="w-full bg-gray-800 overflow-y-auto">
       {lineCommands.map(renderLine)}
-      <p className="text-transparent bg-transparent w-full h-full" onClick={() => inputRef.current.focus()}/>
+      <p className="text-transparent bg-transparent w-full h-full" onClick={handleFocus}/>
     </div>
   );
 }
